6fe716f9864fbdc38a73a8b451e2e3ce9a22904a
comdiv <fagim.sadykov@gmail.com>
2022-11-06 22:54:48 +0500

    добавил парсер TestDesc

---
+++ src/main/kotlin/DocType.kt
@@ -0,0 +1,15 @@
+enum class DocType {
+    UNDEFINED,
+    PASSPORT_RF,
+    DRIVER_LICENSE,
+    VIN,
+    STS,
+    PTS,
+    INN_FL,
+    INN_UL,
+    EGRN,
+    EGRIP,
+    SNILS,
+    TEST_TYPE_1,
+    TEST_TYPE_2
+}
---
+++ src/main/kotlin/ExpectedDocument.kt
@@ -0,0 +1,24 @@
+import kotlinx.serialization.Serializable
+
+/**
+ * Описание извлеченного документа
+ * */
+@Serializable
+data class ExtractedDocument(
+    val docType: DocType = DocType.UNDEFINED,
+    val isValid: Boolean = true,
+    val value: String = ""
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is ExtractedDocument) {
+            return internalEqual(other as ExtractedDocument)
+        }
+
+        return super.equals(other)
+    }
+
+    private fun internalEqual(comparedAnswer: ExtractedDocument): Boolean {
+        return comparedAnswer.docType == docType
+                && comparedAnswer.value == value
+    }
+}
